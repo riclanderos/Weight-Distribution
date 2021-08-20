0cd71efeeafc9d7f816729930c1b615824f7cfd8
Ricardo Landeros <landerosr81@gmail.com>
2021-08-19 19:48:18 -0700

    function for finding array average

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+const skidBoxWeight = [
+  700, 500, 620, 460, 230, 500, 450, 550, 830, 420, 340, 190, 3110,
+];
+let avg = averageArray(skidBoxWeight);
+let sum = 0;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const bigBoxWeight = [
+  650, 780, 840, 340, 150, 540, 360, 790, 620, 430, 480, 560,
+];
+for (let i = 0; i < skidBoxWeight.length; i++) {
+  sum += skidBoxWeight[i];
 }
 
-export default App;
+function averageArray(ar) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  let skidBoxWeight = ar.length;
+  let sum = ar.reduce(reducer);
+
+  console.log(sum / skidBoxWeight);
+}
+
+console.log(sum);
+
+if (sum >= 10000) {
+  console.log("Over weight limit");
+} else if (sum >= 9000 && sum <= 10000) {
+  console.log("Weight is Acceptable");
+} else {
+  console.log("Need more weight");
+}
